0e3b327a48b47aabd27bc15bb4c7285e618e1074
raghu1813 <raghuramk33@gmail.com>
2019-11-27 20:59:08 +0530

    changes

--- RSAattack1.py
+++ RSAattack1.py
@@ -1,43 +1,38 @@
+def p_and_q(n):
+    data = []
+    for i in range(2, n):
+        if n % i == 0:
+            data.append(i)
+    return tuple(data)
 
-from fractions import gcd
 
-def egcd(a, b):
-    if a == 0:
-        return b, 0, 1
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
+def euler(p, q):
+    return (p - 1) * (q - 1)
 
-def modinv(a, m):
-    g, x, y = egcd(a, m)
-    if g != 1:
-        raise ValueError('Modular inverse does not exist.')
-    else:
-        return x % m
 
-def attack(c1, c2, e1, e2, N):
-    if gcd(e1, e2) != 1:
-        raise ValueError("Exponents e1 and e2 must be coprime")
-    s1 = modinv(e1,e2)
-    s2 = (gcd(e1,e2) - e1 * s1) / e2
-    temp = modinv(c2, N)
-    m1 = pow(c1,s1,N)
-    m2 = pow(temp,-s2,N)
-    return (m1 * m2) % N
+def private_index(e, euler_v):
+    for i in range(2, euler_v):
+        if i * e % euler_v == 1:
+            return i
+
+
+def decipher(d, n, c):
+    return c ** d % n
 
 def main():
+    e = int(input("input e: "))
+    n = int(input("input n: "))
+    c = int(input("input c: "))
 
-    ct1=int(input("enter cipher text1"))
-    ct2=int(input("enter cipher text2"))
-    e1=int(input("enter e1"))
-    e2=int(input("enter e2"))
-    cmod=int(input("enter common modulus"))
-    try:
-        message = attack(ct1,ct2,e1,e2,cmod)
-        print("Attack finished!")
-        print('\nPlaintext message:\n%s' % format(message, 'x').decode('hex'))
-    except Exception as e:
-        print('[+] Attack failed!')
-        print(e.message)
-if __name__ == '__main__':
+# t = 123
+# private key = (103, 143)
+    p_and_q_v = p_and_q(n)
+# print("[p_and_q]: ", p_and_q_v)
+    euler_v = euler(p_and_q_v[0], p_and_q_v[1])
+
+# print("[euler]: ", euler_v)
+    d = private_index(e, euler_v)
+    plain = decipher(d, n, c)
+    print("plain: ", plain)
+if __name__ == "__main__":
     main()
