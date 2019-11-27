b8e72ebe825522d8074053e5afa66352053a07f5
raghu1813 <raghuramk33@gmail.com>
2019-11-27 16:50:53 +0530

    changes

--- RSAattack1.py
+++ RSAattack1.py
@@ -3,7 +3,7 @@ from fractions import gcd
 
 def egcd(a, b):
     if a == 0:
-        return (b, 0, 1)
+        return b, 0, 1
     else:
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
